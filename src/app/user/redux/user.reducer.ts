d8f53543286ba8bdd731edd79a070ec018fa2d73
Daniel Kahle <daniel.kahle@acxiom.com>
2017-08-29 16:51:15 -0700

    no-epics: done

--- src/app/user/redux/user.reducer.ts
+++ src/app/user/redux/user.reducer.ts
@@ -8,17 +8,15 @@ export function userReducer(state: IUserState = USER_STATE_INITIAL, action) {
     case UserActions.GET_USER_SUCCESS:
       return {...state, user: action.payload};
     case UserActions.ADD_USER:
-      return {...state, addedUser: false, addUser: action.payload};
+      return {...state, addUser: action.payload};
     case UserActions.ADD_USER_SUCCESS:
-      return {...state, addedUser: true, addUser: action.payload};
+      return {...state, addUser: action.payload};
     case UserActions.UPDATE_USER:
-      return {...state, updatedUser: false, updateUser: action.payload};
+      return {...state, updateUser: action.payload};
     case UserActions.UPDATE_USER_SUCCESS:
-      return {...state, updatedUser: true, updateUser: action.payload};
+      return {...state, updateUser: action.payload};
     case UserActions.DELETE_USER:
-      return {...state, deletedUser: false, deleteUserId: action.meta.id};
-    case UserActions.DELETE_USER_SUCCESS:
-      return {...state, deletedUser: true, numDeleted: action.payload.count};
+      return {...state, deleteUserId: action.meta.id};
     default:
       return state
   }
